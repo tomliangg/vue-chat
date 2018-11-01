7850fa08e662b7ec2570893802b338ad785e2fdc
Tom Liang <tomliangg@gmail.com>
2018-10-31 23:44:19 -0700

    passing props via routes

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,12 @@ export default new Router({
       path: '/',
       name: 'Welcome',
       component: Welcome
+    },
+    {
+      path: '/chat',
+      name: 'Chat',
+      component: Chat,
+      props: true
     }
   ]
 })
