99964f8c18f881df0321aedf995fc49533a77a67
Tom Liang <tomliangg@gmail.com>
2018-11-02 09:51:07 -0700

    adding msg to firestore

--- src/components/NewMessage.vue
+++ src/components/NewMessage.vue
@@ -3,26 +3,43 @@
     <form @submit.prevent="addMessage">
       <label for="new-message">New message (enter to add):</label>
       <input type="text" name="new-message" v-model="newMessage">
+      <p v-if="feedback" class="red-text">{{ feedback }}</p>
     </form>
   </div>
 </template>
 
 <script>
+import db from '@/firebase/init'
+
 export default {
   name: 'NewMessage',
   props: ['name'],
   data(){
     return{
-      newMessage: null
+      newMessage: null,
+      feedback: null
     }
   },
   methods: {
     addMessage(){
-      console.log(this.newMessage, this.name, Date.now())
+      if(this.newMessage){
+        db.collection('messages').add({
+          content: this.newMessage,
+          name: this.name,
+          timestamp: Date.now()
+        }).catch(err => {
+          console.log(err)
+        })
+        this.newMessage = null
+        this.feedback = null
+      } else {
+        this.feedback = 'You must enter a message in order to send one'
+      }
     }
   }
 }
 </script>
 
 <style>
+
 </style>
