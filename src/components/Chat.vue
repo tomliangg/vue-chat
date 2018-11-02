2cd9c41f1dd20ca3ad68334ca99ea3dffff230a4
Tom Liang <tomliangg@gmail.com>
2018-11-02 15:15:16 -0700

    formatting with momentJS; but webpack dev server is failing

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -46,7 +46,7 @@ export default {
             id: doc.id,
             name: doc.data().name,
             content: doc.data().content,
-            timestamp: doc.data().timestamp
+            timestamp: moment(doc.data().timestamp).format('lll')
           })
         }
       })
@@ -65,7 +65,7 @@ export default {
 }
 .chat .time{
   display: block;
-  font-size: 1.2em;
+  font-size: 0.9em;
 }
 </style>
 
